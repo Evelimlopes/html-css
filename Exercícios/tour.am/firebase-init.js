a5d108b659f98e28cc56215f4c70344ca9ba078a
Evelimlopes <99093267+Evelimlopes@users.noreply.github.com>
2022-11-07 21:30:35 -0400

    mais arquivos
    nos arquivos: login cadastro etc.

---
+++ Exercícios/tour.am/firebase-init.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCmmAj6an5BtXyeK-zXiYPRkvZSpjcPJ5I",
+    authDomain: "tour-9a21d.firebaseapp.com",
+    projectId: "tour-9a21d",
+    storageBucket: "tour-9a21d.appspot.com",
+    messagingSenderId: "218860401182",
+    appId: "1:218860401182:web:b8d290b4f754fd6d37b40f"
+  };
+firebase.initializeApp(firebaseConfig);
